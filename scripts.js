d2e270e3ebae3e56af375b0d6e0ab4e926c40e70
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-11-17 18:45:21 +0000

    Add core header functionlity
    Added a desktop grid layout header
    Added a responsive offcanvas header menu for tablet and smaller screens
    Added container width mega menus samples
    Add a bootstrap search bar placeholder
    Added accout and basket icons, links and text
    Added a new hamburger menu from boostrap icons

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 console.log("stylesheet conencted");
+
+$(document).ready(function () {
+    $('.navbar-light .dmenu').hover(function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+        }, function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+        });
+    }); 
+     
+        $(document).ready(function() {
+        $(".megamenu").on("click", function(e) {
+            e.stopPropagation();
+        });
+    });
+    
+   /*  $(document).ready(function() {
+        $('.navbar-toggler').click(function() {
+          $('#offcanvasExample').toggleClass('open');
+        });
+      }); */

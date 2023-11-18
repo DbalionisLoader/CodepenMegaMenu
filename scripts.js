6f43e0c3415962349ab10fa614f9f2bd25b328c9
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-11-18 14:59:19 +0000

    Change header style
    Added site logo
    Adjusted mobile hamburger menu position, size and added text title
    Adjusted seaach length for both desktop and smaller screen
    Changed search button styling
    Added a hr top margin to header
    Added bootstrap icons, for seach bar buttons and hamburger menu

--- scripts.js
+++ scripts.js
@@ -14,8 +14,3 @@ $(document).ready(function () {
         });
     });
     
-   /*  $(document).ready(function() {
-        $('.navbar-toggler').click(function() {
-          $('#offcanvasExample').toggleClass('open');
-        });
-      }); */
